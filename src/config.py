1498dbe08aaa1a4231361c037411cd8607e876f0
Nikola Selic <selich.work@gmail.com>
2020-08-12 22:19:13 +0200

    Run model

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    def __init__(self, mode='conv', nfilt=26, nfeat=13, nfft=512, rate=16000):
+        self.mode = mode
+        self.nfilt = nfilt
+        self.nfeat = nfeat
+        self.nfft = nfft
+        self.rate = rate
+        self.step = int(rate / 10)
+        self.model_path = os.path.join('../models', mode + '.model')
+        # pickle file
+        self.p_path = os.path.join('../data/pickles', mode + '.p')
---
+++ src/plots.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+
+
+def plot_signals(signals):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(signals.keys())[i])
+            axes[x,y].plot(list(signals.values())[i])
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_fft(fft):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            data = list(fft.values())
+            Y, freq = data[0] , data[1]
+            axes[x,y].set_title(list(fft.keys())[i])
+            axes[x,y].plot(freq, Y)
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_fbank(fbank):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(fbank.keys())[i])
+
+            axes[x,y].imshow(list(fbank.values())[i], cmap='hot', interpolation='nearest')
+
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_mfccs(mfccs):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(fbank.keys())[i])
+
+            axes[x,y].imshow(list(mfccs.values())[i], cmap='hot', interpolation='nearest')
+
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
